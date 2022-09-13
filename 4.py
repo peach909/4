2cc4929cb8b84db803be7897c19d0cc978df8554
peach909 <99386746+peach909@users.noreply.github.com>
2022-09-13 20:47:25 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,7 @@
+Python 3.10.3 (tags/v3.10.3:a342a49, Mar 16 2022, 13:07:40) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a = [1, 2, 3, 4, 5]
+print(a)
+[1, 2, 3, 4, 5]
+a.clear()
+print(a)
